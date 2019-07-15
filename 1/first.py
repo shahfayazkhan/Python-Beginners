c7a7a634fc74396adf188b91395d78c28ae47286
Shah Fayaz Khan <40967624+shahfayazkhan@users.noreply.github.com>
2019-07-16 00:11:24 +0500

    Python-Beginners First
    Python-Beginners First Class

---
+++ 1/first.py
@@ -0,0 +1,27 @@
+x = 2.2
+y = '2.2'
+
+z = 5
+
+print("x is : "+str(x))
+print("y is : "+str(y))
+print("z is : "+str(z))
+
+def total():
+    print("sfk")
+
+def second():
+    total()
+
+second()
+
+"""
+first assignment : 
+make a function for every category
+name : shahfayaz khan
+father : khial badshah
+age 24
+mobile no: 03465675304
+address : peshawar
+    thanks
+"""
