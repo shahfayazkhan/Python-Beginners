02a027448ee9eda896c50ec78cd79da3287d1131
Shah Fayaz Khan <40967624+shahfayazkhan@users.noreply.github.com>
2019-07-17 02:03:32 +0500

    Python Second Class Code
    this is Python beginners course and this one is the second class code

---
+++ Second/Functions.py
@@ -0,0 +1,30 @@
+def name():
+    return "shahfayaz khan"
+
+def father_name():
+    #print(name())
+    
+    #print("khial badshah")
+    return "father name"
+
+def age():
+    return 28
+
+def mobile_no():
+    #print(age())
+    #print("03465675304")
+    return 3465675304
+
+def finish():
+    #mobile_no()
+    #father_name()
+    return "Finished"
+
+def main():
+    print("Name : "+name()+"\nFather Name : "+father_name()+"\nAge : "+str(age())+"\n"
+                "Mobile No : "+str(mobile_no())+"\n\t\t"+finish())
+
+#mn()
+main()
+
+
