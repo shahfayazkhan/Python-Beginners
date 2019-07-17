2bb6abe5a172a8578608dacaf5952a9f5a4bdbd0
Shah Fayaz Khan <40967624+shahfayazkhan@users.noreply.github.com>
2019-07-18 01:10:21 +0500

    Python Beginners in Pashto
    Python Beginners Third class is taking input from user and give a output result

---
+++ 3-UserInput/userinput.py
@@ -0,0 +1,26 @@
+# x = float(input('Enter your age : '))
+
+# print("khan : "+str(x))
+
+# def user_input(value):
+#     return input(value)
+
+# def name():
+#     return user_input("Enter your name : ")
+
+# def father_name():
+#     return user_input("Enter Father name : ")
+
+# print("Your name is : "+name()+"\nFather name : "+father_name())
+
+name = input("enter name : ")
+fname = input("Enter father name : ")
+age = int(input("age : "))
+
+print("name : "+name+"\nfather name : "+fname+"\nage: "+str(age))
+
+"""
+create portfolio for user
+but also used in function for this
+showing in beautiful way
+"""
